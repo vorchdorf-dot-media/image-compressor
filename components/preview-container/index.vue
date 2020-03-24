05a836137fb6375258264c67a340ee33dc527f9f
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-03-24 13:48:19 +0100

    fix: fixed display issues in preview container

--- components/preview-container/index.vue
+++ components/preview-container/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="clear" class="container">
+  <div v-if="clear" :class="{ clear }" class="container">
     <Dropzone />
     <div class="link-container">
       <a :href="url" rel="noopener noreferrer" target="_blank">Source Code</a>
@@ -43,14 +43,17 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .container {
   position: relative;
-  display: grid;
   grid-area: main;
-  grid-template-rows: minmax(0, 1fr) auto;
   height: 100%;
   width: 100%;
   background-repeat: repeat;
   background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="-4 -4 8 8" fill="rgba(255, 255, 255, 0.05)" height="32" width="32"><circle r=".5"/><path d="M-2-.125h1v.25h-1zM1-.125h1v.25H1zM-.125-2h.25v1h-.25zM-.125 1h.25v1h-.25z"/></svg>');
   overflow: hidden;
+}
+
+.clear {
+  display: grid;
+  grid-template-rows: minmax(0, 1fr) auto;
 }
 
 .link-container {
