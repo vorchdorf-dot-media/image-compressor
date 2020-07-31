ad8950642df6890fbb0f6756f64d129311c5bcb0
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-07-31 15:39:02 +0200

    feat: added maskable icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,6 +104,9 @@ export default {
    ** PWA configuration
    */
   pwa: {
+    icon: {
+      purpose: ['maskable']
+    },
     manifest: {
       name: 'Image Compressor',
       short_name: 'Compressor',
